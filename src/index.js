1f915bda3e72c8557a2797c16b8fd134abea4cca
irenegf83 <irene@irenegf.com>
2019-05-07 18:03:11 +0200

    Add props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MediaCard from './MediaCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+ReactDOM.render(<MediaCard 
+        name='Jean Grey' 
+        date='15 de mayo de 2098' 
+        image='https://i.blogs.es/8c4b43/dark/200_200.jpg'
+        content={content}
+        likes={0}
+        fill=''
+    />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
